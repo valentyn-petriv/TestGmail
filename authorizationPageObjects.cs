65ca21d3a46417249e8784e4d3434b2d07f5a9af
valentyn-petriv <51762577+valentyn-petriv@users.noreply.github.com>
2023-02-08 13:10:22 +0200

    Add files via upload

---
+++ authorizationPageObjects.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestGmail.PageObjects
+{
+    class AuthorizationPageObjects
+    {
+        private IWebDriver _webDriver;
+        private readonly By _loginInput= By.XPath("//input[@name='login']");
+        private readonly By _passwordInput = By.XPath("//input[@name='password']");
+        private readonly By _continueButton = By.XPath("//button[@class='Ol0-ktls jY4tHruE _2yaudugp']");
+        private readonly By _loginUser = By.XPath("//p[@class = 'login-button__user']");
+        public const string _login = "testeremail@ukr.net";
+        public const string _login2 = "testeremail2@ukr.net";
+        public const string _password = "test1234";
+        public AuthorizationPageObjects(IWebDriver webDriver)
+            
+        {
+            _webDriver = webDriver;
+        }
+
+        public msgListPageObjects Login(string login, string password)
+        {
+            _webDriver.FindElement(_loginInput).SendKeys(login);
+            _webDriver.FindElement(_passwordInput).SendKeys(password);
+            Thread.Sleep(400);
+            _webDriver.FindElement(_continueButton).Click();
+            Thread.Sleep(1000);
+            var actualLogin = _webDriver.FindElement(_loginUser).Text;
+            Thread.Sleep(1000);
+            Assert.AreEqual(login, actualLogin, "login is wrong");
+            //Console.WriteLine("qqq");
+;            return new msgListPageObjects(_webDriver);
+
+        }
+       
+    }
+}
---
+++ sendMsg.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestGmail.PageObjects
+{
+    class sendMsg
+    {
+        private readonly By _toInput = By.XPath("//input[@name='toFieldInput']");
+        public const string _login2 = "testeremail2@ukr.net";
+        private readonly By _subjectInput = By.XPath("//input[@name='subject']");
+        // private readonly By _textInput = By.XPath("//span[@id='_mce_caret']");
+        private readonly By _sendButton = By.XPath("//button[@class='button primary send']");
+        public const string subject = "Тестування";
+        public const string text = "Hello";
+        private readonly By _controlButton = By.XPath("//button[@class='login-button__control']");
+        private readonly By _logoutButton = By.XPath("//a[@href='/q/logout']");
+
+        private IWebDriver _webDriver;
+
+        public sendMsg(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+        }
+
+        public sendMsg SendMessage()
+        {
+            Thread.Sleep(1000);
+            _webDriver.FindElement(_toInput).SendKeys(_login2);
+            _webDriver.FindElement(_subjectInput).SendKeys(subject);
+            // _webDriver.FindElement(_textInput).SendKeys(text);
+            _webDriver.FindElement(_sendButton).Click();
+            return new sendMsg(_webDriver);
+        }
+        public AuthorizationPageObjects Logout()
+        {
+            Thread.Sleep(1000);
+            _webDriver.FindElement(_controlButton).Click();
+            Thread.Sleep(1000);
+            _webDriver.FindElement(_logoutButton).Click();
+
+            return new AuthorizationPageObjects(_webDriver);
+        }
+    }
+}
