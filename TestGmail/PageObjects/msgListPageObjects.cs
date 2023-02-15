395e6aa360dff0184bc2061ac84c238954e1299c
валентин петрів <valentyn.petriv12@gmail.com>
2023-02-15 14:35:56 +0200

    Commit1

---
+++ TestGmail/PageObjects/msgListPageObjects.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestGmail.PageObjects
+{
+    class msgListPageObjects
+    {
+        private IWebDriver _webDriver;
+        private readonly By _sendButton = By.XPath("//button[@class='button primary compose']");
+        private readonly By _emailSubject = By.XPath("//a[@class='msglist__row_href']");
+        private const string wrongSubject = "ththtrh";
+
+        public msgListPageObjects(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+        }
+        public sendMsg SendMessage()
+        {
+
+            WaitUntil.WaitElement(_webDriver, _sendButton);
+            _webDriver.FindElement(_sendButton).Click();
+            return new sendMsg(_webDriver);
+
+        }
+
+        public msgListPageObjects Email() 
+            {
+            var actualSubject = _webDriver.FindElement(_emailSubject).Text;
+            Assert.AreEqual(sendMsg.subject, actualSubject, "subject is wrong");
+            return new msgListPageObjects(_webDriver);
+            } 
+
+        public static string RandomString(int size, bool lowerCase)
+        {
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+            if (lowerCase)
+                return builder.ToString().ToLower();
+            return builder.ToString();
+        }
+
+    }
+}
---
+++ TestGmail/UnitTest1.cs
@@ -0,0 +1,68 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using TestGmail.PageObjects;
+using WebDriverManager;
+
+namespace TestGmail
+{
+    public class Tests
+    {
+        private IWebDriver driver;
+       
+       // private const string _ = "testeremail@ukr.net";
+
+
+
+        [SetUp]
+        public void Setup()
+        {
+     
+            driver = new ChromeDriver("D:\\chromedriver_win32\\chromedriver");
+           
+            driver.Navigate().GoToUrl("https://accounts.ukr.net/login?lang=uk");
+            driver.Manage().Window.Maximize();
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var authorization = new AuthorizationPageObjects(driver);
+            authorization.Login(AuthorizationPageObjects._login, AuthorizationPageObjects._password);
+            var send = new msgListPageObjects(driver);
+            send.SendMessage();
+
+            var sendMsg = new sendMsg(driver);
+            sendMsg.SendMessage();
+            sendMsg.Logout();
+            Thread.Sleep(1000);
+            authorization.Login(AuthorizationPageObjects._login2, AuthorizationPageObjects._password);
+
+        }
+
+
+        [Test]
+        public void Test2()
+        {
+            var authorization = new AuthorizationPageObjects(driver);
+            authorization.Login(AuthorizationPageObjects._login, AuthorizationPageObjects._password);
+            var send = new msgListPageObjects(driver);
+            send.SendMessage();
+
+            var sendMsg = new sendMsg(driver);
+            sendMsg.SendMessage();
+            sendMsg.Logout();
+            Thread.Sleep(1000);
+            authorization.Login(AuthorizationPageObjects._login2, AuthorizationPageObjects._password);
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            
+        }
+    }
+}
